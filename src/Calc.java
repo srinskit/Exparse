808d1892b299aaf84424add100bef1e22e319f8c
srinskit <srinskit@github.com>
2017-10-26 22:10:10 +0530

    Added basic structure

--- src/Calc.java
+++ src/Calc.java
@@ -1,2 +1,19 @@
-public class Calc {
+import java.util.ArrayList;
+
+//Evaluates given infix expression
+class Calc {
+    //    Formats output double
+    static String format(String exp) {
+        return exp;
+    }
+
+    //    Method evaluates given expression to double
+    static double eval(String exp) {
+        return exp.length();
+    }
+
+    //    Converts infix to postfix
+    static ArrayList makePostFix(String exp) {
+        return new ArrayList();
+    }
 }
