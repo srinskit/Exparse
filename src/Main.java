83865a7242a06d8a36baa5da61c5cbd357361667
srinskit <srinskit@github.com>
2017-10-28 02:19:23 +0530

    Driver

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Calc.eval("10+10"));
+        String exp = "10x5";
+        LinkedList<Object> list = Calc.parseExpression(exp);
+        System.out.println(list);
+        list = Calc.toPostfix(list);
+        System.out.println(list);
+        try {
+            System.out.println(Calc.eval(list));
+        } catch (Calc.CalcException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
